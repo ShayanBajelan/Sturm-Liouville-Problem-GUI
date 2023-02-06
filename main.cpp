b23818f7c7217255f332949aafc7fc3ff5eb0f5a
Shayan <61280030+ShayanBajelan@users.noreply.github.com>
2023-02-06 18:57:29 +0330

    Delete main.cpp

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "iostream"
-#include "rungekutta.h"
-
-int main() {
-    std::cout << "ay kose madaret khoda\n";
-    std::cout << solve(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16, 14, 15, 16) << std::endl;
-}
